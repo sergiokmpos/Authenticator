38fd17255e3397d82ea73a9bd80b8b2026562407
SERGIO PAIVA DE CAMPOS <32659392+sergiokmpos@users.noreply.github.com>
2024-03-04 16:41:43 +0000

    Primeiros Arquivos

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+authenticator.login()
+
+if st.session_state["authentication_status"]:
+    try:
+        if authenticator.reset_password(st.session_state["username"]):
+            st.success('Password modified successfully')
+    except Exception as e:
+        st.error(e)

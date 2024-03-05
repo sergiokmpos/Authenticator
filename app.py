20079a3212e83d0fa74d01288ea1f407cf84e25f
SERGIO PAIVA DE CAMPOS <32659392+sergiokmpos@users.noreply.github.com>
2024-03-05 16:53:59 +0000

    Entendento o reser pw

--- app.py
+++ app.py
@@ -17,8 +17,23 @@ authenticator = stauth.Authenticate(
 authenticator.login()
 
 if st.session_state["authentication_status"]:
+    authenticator.logout()
+    st.write(f'Bem Vindo *{st.session_state["name"]}*')
+    st.title('Pagina Inicial')
+elif st.session_state["authentication_status"] is False:
+    st.error('Usuario/Senha está incorreto')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Por favor, entre com seu suario e senha')
+
+
+if st.session_state["authentication_status"]:
     try:
         if authenticator.reset_password(st.session_state["username"]):
             st.success('Password modified successfully')
     except Exception as e:
         st.error(e)
+
+
+with open('config.yaml', 'w') as file:
+    yaml.dump(config, file, default_flow_style=False)            
+

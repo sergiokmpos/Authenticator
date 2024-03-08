a500a27e47642b4c65253068b20e0792e7632e89
SERGIO PAIVA DE CAMPOS <32659392+sergiokmpos@users.noreply.github.com>
2024-03-08 17:12:04 +0000

    ajuste layout e idioma

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import streamlit as st
 import streamlit_authenticator as stauth
+import json
 
 import yaml
 from yaml.loader import SafeLoader
 
+# Carrega o arquivo JSON
+with open("i18n.json", "r") as f:
+    i18n = json.load(f)
 
 idiomas = ['Português', 'Inglês', 'Español']
+
+st.sidebar.image("images/logo-250-100-transparente.png")
 
 # Using object notation
 idioma = st.sidebar.selectbox(
@@ -13,20 +19,19 @@ idioma = st.sidebar.selectbox(
     ('Português', 'Inglês', 'Español')
 )
 
-#idioma = st.selectbox('🗺️ Idioma/Language', idiomas)
-
-
 # Cria o widget de redefinição de senha
 
 if idioma == 'Português':
     campos_login = { 'Form name': 'Iniciar Seção','Username': 'Usuário','Password': 'Senha','Login': 'Entrar' }
+    idioma_i18 = "pt"
 
 elif idioma == 'Inglês':
     campos_login = { 'Form name': 'Login','Username': 'User','Password': 'Password','Login': 'Login' }
+    idioma_i18 = "en"
 
 else :
     campos_login = { 'Form name': 'Iniciar sesión','Username': 'Usuario','Password': 'Contraseña','Login': 'Ingresar' }
-
+    idioma_i18 = "es"
 
 
 with open('config.yaml') as file:
@@ -40,6 +45,12 @@ authenticator = stauth.Authenticate(
     config['preauthorized']
 )
 
+title = i18n ["title"].get(idioma_i18, "Translation not available")
+subtitle = i18n ["subtitle"].get(idioma_i18, "Translation not available")
+
+st.title(title)
+st.subheader(subtitle)
+
 
 authenticator.login(fields = campos_login)
 
@@ -50,8 +61,7 @@ if st.session_state["authentication_status"]:
 elif st.session_state["authentication_status"] is False:
     st.error('Usuario ou senha incorreto')
 elif st.session_state["authentication_status"] is None:
-    st.warning('Please enter your username and password')
-
+    st.warning(i18n["Alerta_login"].get(idioma_i18, "Translation not available"))
 
 
 # Using "with" notation
@@ -60,3 +70,10 @@ with st.sidebar:
         "Choose a shipping method",
         ("Standard (5-15 days)", "Express (2-5 days)")
     )
+
+
+st.write("----")
+
+footer = "Developed with Python 🐍"
+
+st.markdown(footer, unsafe_allow_html=True)
